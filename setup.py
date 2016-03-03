2297c0b908aca7d06c91ac642011ed3c128cdbef
Tim Bolender <git@timbolender.de>
2016-03-04 00:25:56 +0100

    Added requirements to setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 # coding=utf-8
 
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt", session=False)
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='calendarsync',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=reqs,
     scripts=['bin/calendarsync'],
     author='Tim Bolender',
     author_email='contact@timbolender.de',
